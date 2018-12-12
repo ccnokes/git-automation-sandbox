650f287734ff2f65fda67835c33e82c559f1494f
Cameron Nokes <ccnokes@gmail.com>
2018-12-11 21:23:10 -0700

    updates

--- bin/pr.sh
+++ bin/pr.sh
@@ -2,19 +2,29 @@
 
 set -e
 
+script_name=$(basename "$0")
 username=''
 password=''
 title=''
 remote_origin=$(git remote get-url --push origin | cut -d ':' -f 2 | sed 's/\.git//')
 current_branch=$(git rev-parse --abbrev-ref HEAD)
 
-while getopts ":u:p:t:" opt; do
+usage() {
+  echo "Usage: $script_name [-u <username>] [-p <password>] [-t <title of PR>] <body of PR>"
+}
+
+while getopts ":u:p:t:h" opt; do
   case $opt in
     u) username="$OPTARG";;
     p) password="$OPTARG";;
     t) title="$OPTARG";;
+    h)
+      usage
+      exit
+    ;;
     \?)
       echo "INVALID OPTARG $OPTARG" >&2
+      usage
       exit 1
     ;;
   esac
@@ -22,6 +32,26 @@ done
 
 shift $((OPTIND - 1))
 
+if [[ $current_branch == 'master' ]]; then
+  echo "You're already on master, create a new branch, push it, and then run this script to open a PR to merge into master"
+  exit 1
+fi
 
-curl --user "$username:$password" -X POST "https://api.github.com/repos/$remote_origin/pulls" -d "{\"title\":\"$title\",\"base\":\"master\",\"head\":\"$current_branch\",\"body\":\"$@\"}"
+if [[ -z $username ]]; then
+  echo "username must be set"
+  usage
+fi
+
+if [[ -z $password ]]; then
+  echo "password must be set"
+  usage
+fi
+
+if [[ -z $title ]]; then
+  echo "title must be set"
+  usage
+fi
+
+
+curl -s --user "$username:$password" -X POST "https://api.github.com/repos/$remote_origin/pulls" -d "{\"title\":\"$title\",\"base\":\"master\",\"head\":\"$current_branch\",\"body\":\"$@\"}"
 
