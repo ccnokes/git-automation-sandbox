3959749bc46411eed2fbbea28f18af3495a67f4a
Cameron Nokes <ccnokes@gmail.com>
2018-12-21 12:41:16 -0700

    update script

--- bin/pr.sh
+++ bin/pr.sh
@@ -3,11 +3,12 @@
 set -e
 
 script_name=$(basename "$0")
+remote_origin=$(git remote get-url --push origin | cut -d ':' -f 2 | sed 's/\.git//')
+current_branch=$(git rev-parse --abbrev-ref HEAD)
+
 username=''
 password=''
 title=''
-remote_origin=$(git remote get-url --push origin | cut -d ':' -f 2 | sed 's/\.git//')
-current_branch=$(git rev-parse --abbrev-ref HEAD)
 
 usage() {
   echo "Usage: $script_name [-u <username>] [-p <password>] [-t <title of PR>] <body of PR>"
@@ -24,7 +25,7 @@ while getopts ":u:p:t:h" opt; do
     ;;
     \?)
       echo "INVALID OPTARG $OPTARG" >&2
-      usage
+      usage >&2
       exit 1
     ;;
   esac
@@ -32,26 +33,39 @@ done
 
 shift $((OPTIND - 1))
 
+# should be on the branch you're PRing
 if [[ $current_branch == 'master' ]]; then
   echo "You're already on master, create a new branch, push it, and then run this script to open a PR to merge into master"
   exit 1
 fi
 
-if [[ -z $username ]]; then
-  echo "username must be set"
-  usage
+# that branch should exist in the remote already
+if ! git ls-remote --exit-code --heads $(git remote get-url --push origin) "refs/heads/$current_branch"; then
+  echo "This branch isn't pushed. First push this branch, then re-run this script." >&2
+  exit 1
 fi
 
-if [[ -z $password ]]; then
-  echo "password must be set"
-  usage
-fi
+check_is_set() {
+  if [[ -z $2 ]]; then
+    echo "ERROR! $1 must be set" >&2
+    usage >&2
+    exit 1
+  fi
+}
 
-if [[ -z $title ]]; then
-  echo "title must be set"
-  usage
-fi
+check_is_set "username" $username
+check_is_set "password" $password
+check_is_set "title" $title
 
+data=$(cat <<-END
+{
+  "title": "$title",
+  "base": "master",
+  "head": "$current_branch",
+  "body": "$@"
+}
+END
+)
 
-curl -s --user "$username:$password" -X POST "https://api.github.com/repos/$remote_origin/pulls" -d "{\"title\":\"$title\",\"base\":\"master\",\"head\":\"$current_branch\",\"body\":\"$@\"}"
+curl -s --user "$username:$password" -X POST "https://api.github.com/repos/$remote_origin/pulls" -d "$data" > /dev/null
 
